e9f84a3c6f2cca43ae917baf0f3ab799593175cd
FlorianSchieber <81633312+FlorianSchieber@users.noreply.github.com>
2023-04-26 16:58:49 +0200

    algo ex 3.2

---
+++ 01 - Algo/01-introduction/ex_3_1_Controler_saisie/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ex_3_1_Controler_saisie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string prenom;
+
+            do
+            {
+                Console.WriteLine("Bonjour mon chou, quel est ton prénom ?");
+                prenom = Console.ReadLine();
+            }
+            while (prenom.Length < 2);
+            Console.WriteLine("Bonjour " + prenom);
+        }
+    }
+}
---
+++ 01 - Algo/01-introduction/ex_3_2_Controler_saisie_limite/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ex_3_2_Controler_saisie_limite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
