650f8106d2b27df952e8782116a8fde81d10802a
FlorianSchieber <81633312+FlorianSchieber@users.noreply.github.com>
2023-04-28 10:41:35 +0200

    algo partie 3 finie

---
+++ 01 - Algo/01-introduction/ex_3_7_Barnabé/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ex_3_7_Barnabé
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double startSoldEuro;
+            int remainingSoldCent;
+            int shops;
+
+            Console.Write("De combien d'argent dispose Barnabé ? : ");
+            startSoldEuro = double.Parse(Console.ReadLine());
+            remainingSoldCent = (int)(startSoldEuro * 100);
+            shops = 0;
+            while (remainingSoldCent > 0)
+            {
+                if (remainingSoldCent / 2 < 100)
+                {
+                    remainingSoldCent = 0;
+                }
+                else
+                {
+                    remainingSoldCent = remainingSoldCent / 2 - 100;
+                }
+                shops++;
+            }
+            Console.Write("Barnabé a acheté dans " + shops + " magasin(s).");
+        }
+    }
+}
