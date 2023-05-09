538c94cefabb9ad3be573c9bce9b1c29b933a82b
FlorianSchieber <81633312+FlorianSchieber@users.noreply.github.com>
2023-05-09 16:33:03 +0200

    algo ex 5.2 fini

---
+++ 01 - Algo/01-introduction/ex_5_1_Parfaits/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ex_5_1_Parfaits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int limit;
+            int perfectNumberCount;
+            int perfectNumber;
+
+            Console.Write("Veuillez saisir la limite de calcul : ");
+            limit = int.Parse(Console.ReadLine());
+            perfectNumberCount = 1;
+            perfectNumber = 1;
+            while (perfectNumberCount <= limit)
+            {
+                int divisorSum = 0;
+                for (int divisor = 1; divisor < perfectNumber; divisor++)
+                {
+                    if (perfectNumber % divisor == 0)
+                    {
+                        divisorSum += divisor;
+                    }
+                }
+                if (perfectNumber == divisorSum)
+                {
+                    Console.WriteLine(perfectNumber + " est un nombre parfait.");
+                    perfectNumberCount++;
+                }
+                perfectNumber++;
+            }
+
+        }
+    }
+}
---
+++ 01 - Algo/01-introduction/ex_5_2_Conversion/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace ex_5_2_Conversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dimension;
+            string input;
+            bool unitIsKm;
+            bool validInput;
+            bool exit;
+
+            const double MI_IN_KM = 1.609; 
+
+            unitIsKm = true;
+            validInput = false;
+            exit = false;
+
+            do
+            {
+                Console.Write("Veuillez saisir un nombre entre 0.01 et 1 000 000 à convertir (éventuellement suivi d'une unité [km/mi]) : ");
+                input = Console.ReadLine();
+                if (input == "q")
+                {
+                    Console.WriteLine("Au revoir !");
+                    validInput = true;
+                    exit = true;
+                }
+                else
+                {
+                    unitIsKm = true;
+                    if (input.Length >= 2)
+                    {
+                        
+                        if (input.Substring(input.Length -2, 2).ToLower() == "km")
+                        {
+                            input = input.Substring(0, input.Length - 2);
+                        }
+                        else if (input.Substring(input.Length - 2, 2).ToLower() == "mi")
+                        {
+                            unitIsKm  = false;
+                            input = input.Substring(0, input.Length - 2);
+                        }
+                    }
+                    if (double.TryParse(input, out dimension))
+                    {
+                        if (dimension <= 1000000 && dimension >= 0.01)
+                        {
+                            validInput = true;
+                            if (unitIsKm)
+                            {
+                                Console.WriteLine(dimension + " kilomètre(s) = " + dimension / MI_IN_KM + " mile(s)");
+                            }
+                            else
+                            {
+                                Console.WriteLine(dimension + " mile(s) = " + dimension * MI_IN_KM + " kilomètre(s)");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Le nombre n'est pas dans la plage !");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Votre saisie est incorrecte !");
+                    }
+                }
+            }
+            while (!(validInput || exit));
+        }
+    }
+}
