908036becf329f35c3207651928d1144cc16252f
FlorianSchieber <81633312+FlorianSchieber@users.noreply.github.com>
2023-05-15 15:55:22 +0200

    algo ex 6.1

---
+++ 01 - Algo/01-introduction/ex_6_1_Fruits_legumes/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string inputName;
+        double inputPrice;
+        List<string> vegetablesNames = new List<string>();
+        List<double> vegetablesPrices = new List<double>();
+        
+
+        do
+        {
+            Console.Write("Veuillez saisir un légume ('go' pour la suite) : ");
+            inputName = Console.ReadLine();
+            if (inputName != "go")
+            {
+                vegetablesNames.Add(inputName);
+                Console.Write("Veuillez saisir le prix au kilo : ");
+                inputPrice = double.Parse(Console.ReadLine());
+                vegetablesPrices.Add(inputPrice);
+            }
+        }
+        while (inputName != "go");
+        int indexMin = 0;
+        for (int index = 0; index < vegetablesNames.Count; index++)
+        {
+            Console.WriteLine("1 kilogramme de " + vegetablesNames[index] + " coûte " + vegetablesPrices[index] + " euros.");
+            if (vegetablesPrices[index] < vegetablesPrices[indexMin])
+            {
+                indexMin = index;
+            }
+        }
+        Console.WriteLine("Légume le moins cher au kilo : " + vegetablesNames[indexMin]);
+    }
+}
