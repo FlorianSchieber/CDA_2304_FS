a92b22682b16391d479c1dc23816ef728959f8b9
FlorianSchieber <81633312+FlorianSchieber@users.noreply.github.com>
2023-06-26 10:01:21 +0200

    objet ex1 compte bancaire

---
+++ 02 - Objet/02-poo/Ex01_CompteBancaire/CompteBancaire/CL_CompteBancaire/CompteBancaire.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CL_CompteBancaire
+{
+    public class CompteBancaire
+    {
+        private uint id;
+        private string nomProprietaire;
+        private string devise;
+        private int soldeEnDevise;
+        private int decouvertAutoriseEnDevise;
+
+
+        public CompteBancaire(uint _id)
+        {
+            this.id = _id;
+            this.nomProprietaire = "";
+            this.devise = "";
+            this.soldeEnDevise = 0;
+            this.decouvertAutoriseEnDevise = 0;
+        }
+
+        public CompteBancaire(uint _id, string _nomProprietaire, string _devise, int _soldeEnDevise, int _decouvertAutoriseEnDevise)
+        {
+            if (_decouvertAutoriseEnDevise <= 0)
+            {
+                this.id = _id;
+                this.nomProprietaire = _nomProprietaire;
+                this.devise = _devise;
+                this.soldeEnDevise = _soldeEnDevise;
+                this.decouvertAutoriseEnDevise = _decouvertAutoriseEnDevise;
+            }
+            else
+            {
+                throw new Exception("Le découvert autorisé doit être négatif ou nul");
+            }
+        }
+
+        public void Crediter(uint montantEnDevise)
+        {
+            this.soldeEnDevise += (int) montantEnDevise;
+        }
+
+        public bool Debiter(uint montantEnDevise)
+        {
+            int nouveauSoldeEnDevise = this.soldeEnDevise - (int) montantEnDevise;
+            
+            if (nouveauSoldeEnDevise >= this.decouvertAutoriseEnDevise)
+            {
+                this.soldeEnDevise = nouveauSoldeEnDevise;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool TransfererVers(CompteBancaire compteCrediteur, uint montantTransfereEnDevise)
+        {
+            if (this.Debiter(montantTransfereEnDevise))
+            {
+                compteCrediteur.Crediter(montantTransfereEnDevise);
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool EstSuperieurA(CompteBancaire compteAComparer)
+        {
+            return this.soldeEnDevise > compteAComparer.soldeEnDevise;
+        }
+
+        public string DonneInformations()
+        {
+            return "ID : " + this.id +
+                " | Propriétaire : " + this.nomProprietaire +
+                " | Solde : " + this.soldeEnDevise + " " + devise +
+                " | Découvert autorisé : " + this.decouvertAutoriseEnDevise + " " + devise;
+        }
+
+        public override string ToString()
+        {
+            return this.DonneInformations();
+        }
+    }
+}
---
+++ 02 - Objet/02-poo/Ex01_CompteBancaire/CompteBancaire/Pj_CompteBancaire/Program.cs
@@ -0,0 +1,45 @@
+﻿using CL_CompteBancaire;
+
+namespace Pj_CompteBancaire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CompteBancaire c = new CompteBancaire(0);
+            CompteBancaire c0 = new CompteBancaire(12345, "toto", "Eur", 1000, -500);
+            Console.WriteLine(c);
+            Console.WriteLine(c0);
+
+            CompteBancaire b = new CompteBancaire(545454, "Laurent", "Eur", 2000, -500);
+            b.Crediter(100);
+            Console.WriteLine(b);
+            bool ok = b.Debiter(100000);
+            if (ok)
+            {
+                Console.WriteLine("Débit réussi !");
+            }
+            else
+            {
+                Console.WriteLine("Débit échoué !");
+            }
+
+            CompteBancaire c1 = new CompteBancaire(12345, "toto", "Eur", 1000, -500);
+            CompteBancaire c2 = new CompteBancaire(45657, "titi", "Eur", 2000, -1000);
+            c1.TransfererVers(c2, 1300);
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+
+            CompteBancaire c3 = new CompteBancaire(12345, "toto", "USD", 1000, -500);
+            CompteBancaire c4 = new CompteBancaire(45657, "titi", "USD", 2000, -1000);
+            if (c3.EstSuperieurA(c4))
+            {
+                Console.WriteLine("Supérieur !");
+            }
+            else
+            {
+                Console.WriteLine("Inférieur !");
+            }
+        }
+    }
+}
