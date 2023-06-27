f36af8abd702127a7e0b557bf0af545f5ece6ca3
FlorianSchieber <81633312+FlorianSchieber@users.noreply.github.com>
2023-06-27 14:42:19 +0200

    objet ex4 banque

---
+++ 02 - Objet/02-poo/Ex04_Banque/Banque/CL_Banque/Banque.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CL_Banque
+{
+    public class Banque
+    {
+        private string nom;
+        private string ville;
+
+        private List<CompteBancaire> mesComptes;
+
+        public Banque(string _nom, string _ville)
+        {
+            this.nom = _nom;
+            this.ville = _ville;
+            this.mesComptes = new List<CompteBancaire> {};
+        }
+
+        private bool PossedeId(uint idRecherche)
+        {
+            int indiceCompte = 0;
+            bool idSimilaire = false;
+
+            while (indiceCompte < this.mesComptes.Count() && !idSimilaire)
+            {
+                if (idRecherche == this.mesComptes[indiceCompte].DonneId())
+                {
+                    idSimilaire = true;
+                }
+                indiceCompte++;
+            }
+
+            return idSimilaire;
+        }
+
+        private bool PasDeCompte()
+        {
+            return this.mesComptes.Count() == 0;
+        }
+
+        public bool AjouterCompte(uint _id, string _nomProprietaire, string _devise, int _soldeEnDevise, int _decouvertAutoriseEnDevise)
+        {
+            if (!this.PossedeId(_id))
+            {
+                this.mesComptes.Add(new CompteBancaire(_id, _nomProprietaire, _devise, _soldeEnDevise, _decouvertAutoriseEnDevise));
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool AjouterCompte(CompteBancaire compteAAjouter)
+        {
+            if (!this.PossedeId(compteAAjouter.DonneId()))
+            {
+                this.mesComptes.Add(compteAAjouter);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void CompteSuperieur()
+        {
+            if (!this.PasDeCompte())
+            {
+                CompteBancaire compteMax = this.mesComptes[0];
+                
+                for (int indiceCompte = 1; indiceCompte < this.mesComptes.Count(); indiceCompte++)
+                {
+                    if (mesComptes[indiceCompte].EstSuperieurA(compteMax))
+                    {
+                        compteMax = mesComptes[indiceCompte];
+                    }
+                }
+
+                Console.WriteLine(compteMax);
+            }
+            else
+            {
+                Console.WriteLine(this.nom + "n'a pas de compte");
+            }
+        }
+
+        public CompteBancaire? DonneCompte(uint idRecherche)
+        {
+            bool aTrouve = false;
+            int indiceCompte = 0;
+            CompteBancaire compteRecherche = null;
+
+            while (indiceCompte < this.mesComptes.Count() && !aTrouve)
+            {
+                if (idRecherche == this.mesComptes[indiceCompte].DonneId())
+                {
+                    compteRecherche = this.mesComptes[indiceCompte];
+                }
+                indiceCompte++;
+            }
+
+            return compteRecherche;
+        }
+
+        public bool Transferer(uint idCompteDebiteur, uint idCompteCrediteur, uint montantEnDevise)
+        {
+            CompteBancaire? compteDebiteur = this.DonneCompte(idCompteDebiteur);
+            CompteBancaire? compteCrediteur = this.DonneCompte(idCompteCrediteur);
+
+            if (compteDebiteur != null && compteCrediteur != null)
+            {
+                return compteDebiteur.TransfererVers(compteCrediteur, montantEnDevise);
+            }
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            string resultat = "Nom Banque : " + this.nom +
+                "\nVille Banque : " + this.ville + "\n";
+            for (int indiceCompte = 0; indiceCompte < this.mesComptes.Count(); indiceCompte++)
+            {
+                resultat += this.mesComptes[indiceCompte] + "\n";
+            }
+
+            return resultat;
+        }
+    }
+}
---
+++ 02 - Objet/02-poo/Ex04_Banque/Banque/Pj_Banque/Program.cs
@@ -0,0 +1,49 @@
+﻿using CL_Banque;
+
+namespace Pj_Banque
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Banque b = new Banque("Credit Mututu", "Mulhouse");
+            b.AjouterCompte(1245, "Robert", "Eur", 2000, -300);
+            b.AjouterCompte(2568, "Denis", "Eur", 1000, -300);
+
+            if (b.AjouterCompte(2568, "Denis", "Eur", 1000, -300))
+            {
+                Console.WriteLine("Deux comptes de même id !");
+            }
+            else
+            {
+                Console.WriteLine("Interdit de créer un compte avec un id existant !");
+            }
+
+            Console.WriteLine(b);
+            b.CompteSuperieur();
+
+            Console.WriteLine("Le compte d'id 2568 est : " + b.DonneCompte(2568));
+
+            if (b.Transferer(1245, 2568, 1000))
+            {
+                Console.WriteLine("Transfert effectué");
+            }
+            else
+            {
+                Console.WriteLine("Transfert impossible");
+            }
+
+            if (b.Transferer(1245, 2568, 5000))
+            {
+                Console.WriteLine("Transfert effectué");
+            }
+            else
+            {
+                Console.WriteLine("Transfert impossible");
+            }
+            
+            Console.WriteLine(b);
+
+        }
+    }
+}
